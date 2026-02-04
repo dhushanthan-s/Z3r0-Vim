36f797d034ba4f1dec6ddc8482acfdf557c713aa
Dhushanthan <sdhushanthan052@gmail.com>
2026-02-04 22:57:53 +0530

    catppuccin and bar update

--- lua/plugins/ui/buffer-line.lua
+++ lua/plugins/ui/buffer-line.lua
@@ -3,23 +3,6 @@ return {
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons", -- https://github.com/nvim-tree/nvim-web-devicons
     config = function()
-        local config = require("bufferline")
-        local mocha = require("catppuccin.palettes").get_palette("mocha")
-        config.setup({
-            highlights = require("catppuccin.groups.integrations.bufferline").get({
-                styles = { "italic", "bold" },
-                custom = {
-                    all = {
-                        fill = { bg = "#000000" },
-                    },
-                    mocha = {
-                        background = { fg = mocha.text },
-                    },
-                    latte = {
-                        background = { fg = "#000000" },
-                    },
-                },
-            }),
-        })
+        require("bufferline").setup({})
     end,
 }
